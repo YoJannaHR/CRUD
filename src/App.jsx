aeb54d5cb51cf97fc7387b80cf447df1d727889e
Daniela Hernandez <danielayojanna21@gmail.com>
2022-07-18 18:52:38 -0500

    second commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,60 +1,54 @@
-import { useEffect, useState } from 'react'
-import UsersForm from './components/UsersForm'
-import UsersList from './components/UsersList'
-import axios from 'axios'
-import './App.css'
+import { useEffect, useState } from "react";
+import UsersForm from "./components/UsersForm";
+import UsersList from "./components/UsersList";
+import axios from "axios";
+import "./App.css";
 
 function App() {
+  const [users, setUsers] = useState([]);
+  const [userSelected, setUserSelected] = useState(null);
 
-const [users, setUsers] = useState([])
-const [userSelected, setUserSelected] = useState(null);
+  useEffect(() => {
+    axios
+      .get(`https://users-crud1.herokuapp.com/users/`)
+      .then((res) => setUsers(res.data));
+  }, []);
 
+  const getUsers = () => {
+    axios
+      .get(`https://users-crud1.herokuapp.com/users/`)
+      .then((res) => setUsers(res.data));
+  };
 
-useEffect(()=>{
+  const selectUsers = (Registration) => {
+    setUserSelected(Registration);
+  };
 
-axios
-.get(`https://users-crud1.herokuapp.com/users/`)
-.then((res) => setUsers(res.data));
+  const deleteUsers = (id) => {
+    alert(id);
+    axios
+      .delete(`https://users-crud1.herokuapp.com/users/${id}/`)
+      .then(() => getUsers());
+  };
 
-}, []);
+  const deselectUsers = () => setUserSelected(null);
 
-
-const getUsers = () => {
-  axios
-    .get(`https://users-crud1.herokuapp.com/users/`)
-    .then((res) => setUsers(res.data));
-};
-
-
-const selectUsers = (Registration) => {
- 
-  setUserSelected(Registration);
-};
-
-const deleteUsers = (id) => {
-  alert(id);
-  axios.delete(`https://users-crud1.herokuapp.com/users/${id}/`)
-  .then(()=>getUsers())
-};
-
-const deselectUsers = () => setUserSelected (null);
-
-  console.log(users)
+  console.log(users);
 
   return (
     <div className="App">
-      <UsersList 
-       users = {users}
-       selectUsers={selectUsers}
-       deleteUsers={deleteUsers}
-       />
+      <UsersList
+        users={users}
+        selectUsers={selectUsers}
+        deleteUsers={deleteUsers}
+      />
       <UsersForm
-      getUsers ={getUsers}
-      userSelected = {userSelected}
-      deselectUsers = {deselectUsers}
+        getUsers={getUsers}
+        userSelected={userSelected}
+        deselectUsers={deselectUsers}
       />
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
